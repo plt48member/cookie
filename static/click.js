8ad62c5e2ccf60a731783b423dc5d03bbef0b6b3
plt48member <max@platform48.com>
2026-07-09 15:47:39 +0100

    new update

--- static/click.js
+++ static/click.js
@@ -6,6 +6,12 @@ const clickerLabel = document.getElementById("clickers")
 
 const grandmaLabel = document.getElementById("grandmas")
 const grandmaBtn = document.getElementById("buy-grandma")
+const bakerLabel = document.getElementById("bakers");
+const bakerBtn = document.getElementById("buy-baker");
+
+
+let bakerPrice = 100;
+let bakers = 0;
 let cookies = 0
 let clickers = 1
 let grandmas = 0
@@ -14,6 +20,7 @@ function updateLabels() {
     cookieLabel.innerHTML = "Cookies:" +cookies
     clickerLabel.innerHTML = "clickers:" + clickers
     grandmaLabel.innerHTML = "grandmas:" +  grandmas
+    bakerLabel.innerHTML = "Bakers: " + bakers + " | Cost: " + bakerPrice;
 }
 
 updateLabels()
@@ -56,5 +63,42 @@ setInterval(function () {
 
 
 
+let grandmaPrice = 50;
+
+grandmaBtn.onclick = function () {
+    if (cookies >= grandmaPrice) {
+        cookies -= grandmaPrice;
+        grandmas++;
+
+        grandmaPrice = Math.floor(grandmaPrice * 1.15);
+
+        updateLabels();
+    }
+}
 
 
+clickerBtn.onclick = function () {
+    if (cookies >= clickerPrice) {
+        cookies -= clickerPrice;
+        clickers++;
+
+        clickerPrice = Math.floor(clickerPrice * 1.15);
+
+        updateLabels();
+    }
+}
+bakerBtn.onclick = function () {
+    if (cookies >= bakerPrice) {
+        cookies -= bakerPrice;
+        bakers++;
+
+        bakerPrice = Math.floor(bakerPrice * 1.15);
+
+        updateLabels();
+    }
+}
+
+setInterval(function () {
+    cookies = cookies + grandmas + (bakers * 3);
+    updateLabels();
+}, 1000);
