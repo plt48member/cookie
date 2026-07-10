340049d80d69bab23ea01091ebbd29d32bc63632
plt48member <max@platform48.com>
2026-07-10 11:15:56 +0100

    second release

--- static/click.js
+++ static/click.js
@@ -1,75 +1,158 @@
-const cookieBtn = document.getElementById("cookie")
-const cookieLabel = document.getElementById("cookies")
+const cookieBtn = document.getElementById("cookie");
+const cookieLabel = document.getElementById("cookies");
 
-const clickerBtn = document.getElementById("buy-cursor")
-const clickerLabel = document.getElementById("clickers")
+const clickerBtn = document.getElementById("buy-cursor");
+const clickerLabel = document.getElementById("clickers");
 
-const grandmaLabel = document.getElementById("grandmas")
-const grandmaBtn = document.getElementById("buy-grandma")
+const grandmaLabel = document.getElementById("grandmas");
+const grandmaBtn = document.getElementById("buy-grandma");
 
-const bakerLabel = document.getElementById("bakers")
-const bakerBtn = document.getElementById("buy-baker")
+const bakerLabel = document.getElementById("bakers");
+const bakerBtn = document.getElementById("buy-baker");
 
-let cookies = 0
-let clickers = 1
-let grandmas = 0
-let baker = 0
+const maxLabel = document.getElementById("maxs")
+const maxBtn = document.getElementById("buy-maxs")
+
+const clickerPriceLabel = document.getElementById("clickerPrice");
+const grandmaPriceLabel = document.getElementById("grandmaPrice");
+const bakerPriceLabel = document.getElementById("bakerPrice");
+const maxPriceLabel = document.getElementById("maxsPrice");
+const cpsLabel = document.getElementById("cps");
+
+
+
+let cookies = 0;
+
+let clickers = 1;
+let grandmas = 0;
+let bakers = 0;
+let maxs = 0;
+
+
+let clickerPrice = 10;
+let grandmaPrice = 50;
+let bakerPrice = 100;
+let maxsPrice = 500;
+
+
+let cps = 0;
 
 function updateLabels() {
-    cookieLabel.innerHTML = "Cookies:" + cookies
-    clickerLabel.innerHTML = "clickers:" + clickers
-    grandmaLabel.innerHTML = "grandmas:" +  grandmas
-    bakerLabel.innerHTML = "bakers:" + bakers
+
+   cps = grandmas + (bakers * 3) + (maxs * 10);
+
+    cookieLabel.innerHTML = "Cookies: " + Math.floor(cookies);
+
+    clickerLabel.innerHTML =
+        "Clickers: " + clickers + " | Cost: " + clickerPrice;
+
+    grandmaLabel.innerHTML =
+        "Grandmas: " + grandmas + " | Cost: " + grandmaPrice;
+
+    bakerLabel.innerHTML =
+        "Bakers: " + bakers + " | Cost: " + bakerPrice;
+
+        maxLabel.innerHTML =
+        "Maxs: " + maxs + " | Cost: " + maxsPrice;
+       
+
+    clickerPriceLabel.innerHTML = clickerPrice;
+    grandmaPriceLabel.innerHTML = grandmaPrice;
+    bakerPriceLabel.innerHTML = bakerPrice;
+    maxPriceLabel.innerHTML = maxsPrice;
+
+    cpsLabel.innerHTML = "Cookies Per Second: " + cps;
+
+   
 }
 
-updateLabels()
+updateLabels();
 
-cookieBtn.onclick = function(){
-    cookies = cookies+clickers
-    updateLabels()
+// Click Cookie
 
-    console.log("you have clicked the cookie")
-    console.log(cookies)
+cookieBtn.onclick = function () {
 
-}
+    cookies += clickers;
 
-cookieLabel.onclick = function(){
-    
-    console.log("+1 cookie")
-}
-
-clickerBtn.onclick = function(){
-  if (cookies >= 10) {
-    cookies = cookies -10;
-    clickers++;
     updateLabels();
-}
+
+};
+
+// Buy Clicker
+
+clickerBtn.onclick = function () {
+
+    if (cookies >= clickerPrice) {
+
+        cookies -= clickerPrice;
+
+        clickers++;
+
+        clickerPrice = Math.round(clickerPrice * 1.15);
+
+        updateLabels();
+
+    }
+
+};
+
+// Buy Grandma
+
+grandmaBtn.onclick = function () {
+
+    if (cookies >= grandmaPrice) {
+
+        cookies -= grandmaPrice;
+
+        grandmas++;
+
+        grandmaPrice = Math.round(grandmaPrice * 1.15);
+
+        updateLabels();
+
+    }
+
+};
+
+// Buy Baker
+
+bakerBtn.onclick = function () {
+
+    if (cookies >= bakerPrice) {
+
+        cookies -= bakerPrice;
+
+        bakers++;
+
+        bakerPrice = Math.round(bakerPrice * 1.15);
+
+        updateLabels();
+
+    }
+
+};
+
+
+maxBtn.onclick = function(){
+     if (cookies >= maxsPrice) {
+
+        cookies -= maxsPrice;
+
+        maxs++;
+
+        maxsPrice = Math.round(maxsPrice * 1.15);
+
+        updateLabels();
+
+    }
 }
 
-grandmaBtn.onclick = function(){
-  if (cookies >= 50) {
-    cookies = cookies - 50;
-    grandmas++;
-    updateLabels();
-}
-}
+// Automatic Cookies
 
 setInterval(function () {
-    cookies = cookies + grandmas
-    updateLabels()
-}, 1000);
 
+    cookies += cps;
 
+    updateLabels();
 
-
-bakerBtn.onclick = function (){
-    if(cookies >= 100)
-        cookies = cookies - 100
-    bakers++;
-    updateLabels()
-}
-
-setInterval(function () {
-    cookies = cookies + bakers
-    updateLabels()
-}, 1000);
+},1000);
